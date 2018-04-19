4a23432a5245513b9647b8a3002fbf20245e61c1
balticmsww <balticmsww@gmail.com>
2018-04-19 00:03:02 -0300

    Ajuste de saida de resultados

--- tests/calc.exs
+++ tests/calc.exs
@@ -1,7 +1,7 @@
 defmodule Calc do
 # função de soma
     def sum(a, b) when is_integer(a) and is_integer(b) do
-    a + b
+    "O resultado é #{a + b}"
     end
 
     def sum(_,_) do
@@ -10,7 +10,7 @@ defmodule Calc do
 
 # função de subtração
     def sub(a,b) when is_integer(a) and is_integer(b) do
-     a - b
+    "O resultado é #{a - b}"
     end
 
     def sub(_,_) do
@@ -19,7 +19,7 @@ defmodule Calc do
 
 # função de multiplicação
     def mul(a,b) when is_integer(a) and is_integer(b) do
-     a * b
+    "O resultado é #{a * b}"
     end
 
     def mul(_,_) do
@@ -28,7 +28,7 @@ defmodule Calc do
 
 # função de divisão
     def div(a,b) when is_integer(a) and is_integer(b) do
-     a / b
+    "O resultado é #{a / b}"
     end
 
     def div(_,b) when b==0 do
@@ -41,7 +41,7 @@ defmodule Calc do
 
 # função de potenciação
     def exp(a ,b) when is_integer(a) and is_integer(b) do
-      :math.pow(a,b)
+    "O resultado é #{:math.pow(a,b)}"
     end
 
     def exp(_ ,_) do
